5fe523546287017d494f816180ae6c3565aaf747
Antonio Mendoza Pérez <antmendoza@gmail.com>
2024-04-15 13:23:51 +0200

    wip

--- php/run-withrr/run-workflows.sh
+++ php/run-withrr/run-workflows.sh
@@ -3,7 +3,10 @@
 
 for i in {1..5} ; do
 
-  for x in {1..10}; do tctl workflow start --tq "default" --wt "Parent.greet" --input '602'; done
+  for x in {1..10}; do tctl workflow start --tq "default" --wt "Parent.greet" --input '660'; done
+
+
+  for x in {1..10}; do tctl workflow start --tq "default" --wt "Parent.greet" --input '720'; done
 
   sleep 600
 
