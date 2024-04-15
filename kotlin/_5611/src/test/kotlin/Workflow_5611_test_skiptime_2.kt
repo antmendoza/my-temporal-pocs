618e49dafe99d81de40d27b67dd71f05c4867bb4
Antonio Mendoza Pérez <antmendoza@gmail.com>
2024-04-15 19:14:35 +0200

    repro 5611

---
+++ kotlin/_5611/src/test/kotlin/Workflow_5611_test_skiptime_2.kt
@@ -0,0 +1,57 @@
+package io.antmendoza.samples.ui_wizard
+
+import Workflow_5611
+import Workflow_5611Impl
+import io.mockk.junit5.MockKExtension
+import io.temporal.client.WorkflowClient
+import io.temporal.testing.TestWorkflowEnvironment
+import io.temporal.testing.TestWorkflowExtension
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.RegisterExtension
+import java.time.Duration
+import java.time.Instant
+
+@ExtendWith(MockKExtension::class)
+class Workflow_5611_test_skiptime_2 {
+
+
+    @JvmField
+    @RegisterExtension
+    val testWorkflowExtension: TestWorkflowExtension =
+        TestWorkflowExtension.newBuilder()
+            .setWorkflowTypes(
+                Workflow_5611Impl::class.java,
+            )
+            // .setWorkflowClientOptions(workflowClientOptions)
+            .setUseTimeskipping(true)
+            .useInternalService()
+            .build()
+
+
+    @org.junit.jupiter.api.Test
+    fun testHappyPath(
+        workflow: Workflow_5611,
+        testWorkflowEnvironment: TestWorkflowEnvironment,
+        workflowClient: WorkflowClient
+    ) {
+
+        val execution = WorkflowClient.start(workflow::run)
+
+
+        //repeat(3) {
+        println("[BEFORE SLEEP] Workflow time: ${Instant.ofEpochMilli(testWorkflowEnvironment.currentTimeMillis())}, Clock: ${Instant.now()}")
+        testWorkflowEnvironment.sleep(Duration.ofSeconds(5))
+        println("[AFTER SLEEP ] Workflow time: ${Instant.ofEpochMilli(testWorkflowEnvironment.currentTimeMillis())}, Clock: ${Instant.now()}")
+
+        val result = workflowClient.newUntypedWorkflowStub(execution.workflowId).getResult(String::class.java)
+        println("[Result ]  ${result}")
+
+        Assertions.assertEquals("done", result)
+        //}
+
+
+
+    }
+
+}
