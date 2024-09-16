6c25f695af114f8ea4b44b0d1ead73a1fb12ee3a
Antonio Mendoza Pérez <antmendoza@gmail.com>
2024-09-16 12:26:19 -0700

    reproduction

--- java/_7894/client/src/main/java/com/temporal/demos/temporalspringbootdemo/InitController.java
+++ java/_7894/client/src/main/java/com/temporal/demos/temporalspringbootdemo/InitController.java
@@ -1,24 +1,35 @@
 package com.temporal.demos.temporalspringbootdemo;
 
+import io.grpc.Deadline;
 import io.temporal.api.workflowservice.v1.GetSystemInfoRequest;
+import io.temporal.api.workflowservice.v1.GetSystemInfoResponse;
 import io.temporal.client.WorkflowClient;
+import io.temporal.internal.retryer.GrpcRetryer;
+import io.temporal.serviceclient.RpcRetryOptions;
+import io.temporal.serviceclient.SystemInfoInterceptor;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class InitController {
 
 
     private final WorkflowServiceStubs workflowServiceStubs;
+    private final WorkflowClient workflowClient;
 
     private final RestController restController;
 
-    public InitController(final WorkflowServiceStubs workflowServiceStubs, final WorkflowClient workflowClient, final RestController restController) {
+    public InitController(final WorkflowServiceStubs workflowServiceStubs,
+                          final WorkflowClient workflowClient,
+                          final RestController restController) {
         this.workflowServiceStubs = workflowServiceStubs;
+        this.workflowClient = workflowClient;
         this.restController = restController;
     }
 
@@ -44,8 +55,46 @@ public class InitController {
 
     }
 
+    private void initGetSystemInfo3() {
+        final long millis = workflowServiceStubs.getOptions().getHealthCheckAttemptTimeout().toMillis();
+
+
+        workflowClient.getWorkflowServiceStubs()
+                .blockingStub().withDeadline(Deadline.after(
+                                millis,
+                                //workflowServiceStubs.getOptions().getRpcTimeout().getSeconds(),
+                                TimeUnit.MILLISECONDS
+                        )
+
+                ).getSystemInfo(
+                        GetSystemInfoRequest
+                                .newBuilder()
+                                .build());
+    }
+
+    private void initGetSystemInfo2() {
+
+
+        final Deadline deadline = Deadline.after(workflowServiceStubs.getOptions().getHealthCheckAttemptTimeout().toMillis(),
+                TimeUnit.MILLISECONDS);
+
+        RpcRetryOptions rpcRetryOptions =
+                RpcRetryOptions.newBuilder()
+                        .setExpiration(
+                                Duration.ofMillis(deadline.timeRemaining(TimeUnit.MILLISECONDS)))
+                        .validateBuildWithDefaults();
+        GrpcRetryer.GrpcRetryerOptions grpcRetryerOptions =
+                new GrpcRetryer.GrpcRetryerOptions(rpcRetryOptions, deadline);
+
+        new GrpcRetryer(GetSystemInfoResponse.Capabilities::getDefaultInstance)
+                .retryWithResult(
+                        () -> SystemInfoInterceptor.getServerCapabilitiesOrThrow(this.workflowServiceStubs.getRawChannel(),
+                                deadline),
+                        grpcRetryerOptions);
+
+    }
+
     private void invokeGetSystemInfo() {
-        Date d1 = new Date();
 
 
         WorkflowServiceStubs.newConnectedServiceStubs(workflowServiceStubs.getOptions(),
@@ -55,10 +104,6 @@ public class InitController {
                         .build());
 
 
-        Date d2 = new Date();
-
-        long ms = (d2.getTime() - d1.getTime());
-        System.out.println("first GetSystemInfoRequest ms : " + ms);
     }
 
     private void startWorkflow() {
