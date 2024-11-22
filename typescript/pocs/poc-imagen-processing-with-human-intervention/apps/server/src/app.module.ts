c2d071cc496be8942b0a51b5e285ff2e8bf19a34
Antonio Mendoza Pérez <antmendoza@gmail.com>
2024-11-22 10:12:35 +0100

    wip

--- typescript/pocs/poc-imagen-processing-with-human-intervention/apps/server/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ServerModule } from './app/server.module';
-@Module({
-  imports: [ServerModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- typescript/pocs/poc-imagen-processing-with-human-intervention/apps/worker/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ProcessImagesWorkerModule } from './process-img/process-images-worker.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ProcessImagesWorkerModule);
-  await app.listen(3001);
-}
-bootstrap();
