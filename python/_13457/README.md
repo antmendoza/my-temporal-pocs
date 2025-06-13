fd8df178c482a28268f45c45586da5c543b586ed
Antonio Mendoza Pérez <antmendoza@gmail.com>
2025-06-13 18:10:46 +0200

    wip

---
+++ python/_13457/README.md
@@ -0,0 +1,14 @@
+# Context Propagation Interceptor Sample with update
+
+
+    uv python install 3.13
+    
+    uv sync
+
+    uv run worker.py
+
+This will start the worker. Then, in another terminal, run the following to execute the workflow:
+
+    uv run starter.py
+
+![Screenshot 2025-06-13 at 17.46.29.png](Screenshot%202025-06-13%20at%2017.46.29.png)
---
+++ python/_13457/worker.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+
+from temporalio.client import Client
+from temporalio.worker import Worker
+
+import activities, interceptor, workflows
+
+interrupt_event = asyncio.Event()
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+
+    # Connect client
+    client = await Client.connect(
+        "localhost:7233",
+        # Use our interceptor
+        interceptors=[interceptor.ContextPropagationInterceptor()],
+    )
+
+    # Run a worker for the workflow
+    async with Worker(
+        client,
+        task_queue="context-propagation-task-queue",
+        activities=[activities.say_hello_activity],
+        workflows=[workflows.SayHelloWorkflow],
+    ):
+        # Wait until interrupted
+        logging.info("Worker started, ctrl+c to exit")
+        await asyncio.sleep(400)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
