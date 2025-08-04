aa511e809e6ee506acc9b66fadd0345a9c5898f6
Antonio Mendoza Pérez <antmendoza@gmail.com>
2025-08-04 13:50:07 +0200

    wip

---
+++ python/_14722/open_telemetry_with_collector/README.md
@@ -0,0 +1,50 @@
+# OpenTelemetry Sample
+
+
+``` bash
+poetry install --with open_telemetry
+```
+
+Before starting the collector, edit the [collector.yaml](collector%2Fcollector.yaml) file to add the dd api_key
+
+
+```bash
+docker compose down --remove-orphans && docker volume prune -f
+
+docker-compose up 
+
+```
+
+### Otel / Datadog
+
+
+
+
+Start one worker
+
+``` bash
+ps aux | grep worker.py
+
+pkill -f worker.py
+
+export WORKER_ID=100_WORKER___1 
+poetry run python3 worker.py
+```
+
+
+
+Start 50K workflows
+
+``` bash
+export WORKER_ID=CLIENT
+
+# Set the number of workflows to run
+export WORKFLOW_COUNT=500
+poetry run python3 starter.py
+```
+
+
+## references
+https://docs.datadoghq.com/opentelemetry/guide/otlp_delta_temporality/
+
+
---
+++ python/_14722/open_telemetry_with_collector/worker.py
@@ -0,0 +1,104 @@
+import asyncio
+import os
+import random
+from datetime import timedelta
+
+from temporalio import activity, workflow
+from temporalio.client import Client
+from temporalio.contrib.opentelemetry import TracingInterceptor
+from temporalio.runtime import OpenTelemetryConfig, Runtime, TelemetryConfig, OpenTelemetryMetricTemporality, \
+    PrometheusConfig
+from temporalio.worker import Worker
+
+
+@workflow.defn
+class GreetingWorkflow:
+    @workflow.run
+    async def run(self, name: str) -> str:
+        seconds_ = await workflow.execute_activity(compose_greeting, name,
+                                                   start_to_close_timeout=timedelta(seconds=60), )
+
+        print(f"Workflow completing")
+        return seconds_
+
+
+@activity.defn
+async def compose_greeting(name: str) -> str:
+    ## calculate random number smaller than 10
+    random_number = random.randint(1,10)
+    await asyncio.sleep(random_number)
+    return f"Hello, {name}!"
+
+
+interrupt_event = asyncio.Event()
+
+
+def init_runtime_with_telemetry() -> Runtime:
+    ## if env variable prometheus-port is not null setup prometheus
+    prometheus_port = os.environ.get("PROMETHEUS_PORT")
+    if prometheus_port is not None:
+        return Runtime(
+            telemetry=TelemetryConfig(
+                metrics=PrometheusConfig(
+                    bind_address= "127.0.0.1:" + prometheus_port,
+                    histogram_bucket_overrides={
+           #             "temporal_activity_schedule_to_start_latency": [1, 10, 30, 60, 120, 300, 600, 1800, 3600]
+                    }
+                ),
+                global_tags={"anything": "worker_" + prometheus_port},
+            )
+        )
+    else:
+        # Setup SDK metrics to OTel endpoint
+        WORKER_ID = "worker"
+
+        return Runtime(
+            telemetry=TelemetryConfig(
+                metrics=OpenTelemetryConfig(
+                    url="http://localhost:4317",
+                    metric_periodicity=timedelta(seconds=1),
+                    metric_temporality=OpenTelemetryMetricTemporality.DELTA
+                ),
+                global_tags={"anything": "worker_" + WORKER_ID,
+                             "env": "worker_" + WORKER_ID,
+                             "env33": "worker_" + WORKER_ID
+                             },
+
+            )
+        )
+
+
+async def main():
+    runtime = init_runtime_with_telemetry()
+
+    # Connect client
+    client = await Client.connect(
+        "localhost:7233",
+        # Use OpenTelemetry interceptor
+        interceptors=[TracingInterceptor()],
+        runtime=runtime,
+    )
+
+    # Run a worker for the workflow
+    async with Worker(
+            client,
+            task_queue="open_telemetry-task-queue",
+            workflows=[GreetingWorkflow],
+            activities=[compose_greeting],
+            max_concurrent_activities=100,
+    ):
+        # Wait until interrupted
+        print("Worker started, ctrl+c to exit")
+        await interrupt_event.wait()
+        print("Shutting down")
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    interrupt_event = asyncio.Event()  # Now it's bound to this loop
+    try:
+        loop.run_until_complete(main())
+    except KeyboardInterrupt:
+        interrupt_event.set()
+        loop.run_until_complete(loop.shutdown_asyncgens())
