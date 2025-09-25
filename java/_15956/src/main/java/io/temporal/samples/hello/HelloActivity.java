0bcecd26e431a4e141c5552a3c1c0bbda9f8f43d
Antonio Mendoza Pérez <antmendoza@gmail.com>
2025-09-25 13:29:12 +0200

    wip

---
+++ java/_15956/src/main/java/io/temporal/samples/hello/HelloActivity.java
@@ -0,0 +1,203 @@
+package io.temporal.samples.hello;
+
+import io.temporal.activity.*;
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+import io.temporal.common.RetryOptions;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import io.temporal.worker.Worker;
+import io.temporal.worker.WorkerFactory;
+import io.temporal.workflow.*;
+
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloActivity {
+
+    static final String TASK_QUEUE = "HelloActivityTaskQueue";
+
+    static final String WORKFLOW_ID = "HelloActivityWorkflow";
+
+
+
+    @ActivityInterface
+    public interface MyActivities {
+
+        @ActivityMethod
+        String sleep_time(int ms);
+
+        @ActivityMethod
+        String start_workflow(int ms);
+    }
+
+
+
+    @WorkflowInterface
+    public static interface MyWorkflow {
+        @WorkflowMethod
+        String run(String name);
+
+        @UpdateMethod
+        String update();
+
+        @SignalMethod
+        void signal();
+    }
+    // Define the workflow implementation which implements our getGreeting workflow method.
+    public static class MyWorkflowImpl implements MyWorkflow {
+
+
+        private final Logger logger = Workflow.getLogger(MyWorkflowImpl.class.getName());
+
+
+        private final MyActivities activities = Workflow.newActivityStub(
+                MyActivities.class,
+                ActivityOptions.newBuilder()
+                        .setTaskQueue(TASK_QUEUE)
+                        .setStartToCloseTimeout(
+                                Duration.ofMillis(15_000)
+                        )
+                        .setRetryOptions(RetryOptions.newBuilder()
+                                .setBackoffCoefficient(1)
+                                .setInitialInterval(Duration.ofSeconds(5))
+                                .build())
+                        .build());
+
+        private final MyActivities localActivity = Workflow.newLocalActivityStub(
+                MyActivities.class,
+                LocalActivityOptions.newBuilder()
+                        .setStartToCloseTimeout(
+                                //setting to a very large value for demo purpose.
+                                Duration.ofMillis(100_000)
+                        )
+                        .setRetryOptions(RetryOptions.newBuilder()
+                                .setBackoffCoefficient(1)
+                                .build())
+                        .build());
+
+
+
+
+        private CompletablePromise<String> promise = null;
+
+        public String run(String name) {
+
+
+            Async.function(() -> {
+                localActivity.start_workflow(10_000);
+                promise = Workflow.newPromise();
+                return promise;
+            });
+
+
+            localActivity.sleep_time(400);
+            localActivity.sleep_time(400);
+
+
+            //artificially sleep the workflow to allow the local activity to complete.
+            Workflow.sleep(20_000);
+
+
+            return "done";
+
+        }
+
+        @Override
+        public String update() {
+            return null;
+        }
+
+        @Override
+        public void signal() {
+            logger.info("Signal received ...");
+
+            Workflow.await(() -> promise != null);
+
+            logger.info(" promise: " + promise);
+
+            promise.complete(null);
+            logger.info("Processing signal ...");
+
+            activities.sleep_time(1_000);
+
+        }
+
+
+    }
+
+    public static class MyActivitiesImpl implements MyActivities {
+        private static final Logger log = LoggerFactory.getLogger("-");
+
+        @Override
+        public String sleep_time(int ms) {
+
+            try {
+                Thread.sleep(ms);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            return "";
+        }
+
+        @Override
+        public String start_workflow(int ms) {
+
+            log.info("start_workflow activity: Starting a new workflow from activity...");
+
+            sleep_time(ms);
+
+            log.info("start_workflow activity: About to complete ...");
+
+            return "";
+        }
+
+
+    }
+    public static void main(String[] args) {
+
+        WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+
+        WorkflowClient client = WorkflowClient.newInstance(service);
+
+        WorkerFactory factory = WorkerFactory.newInstance(client);
+
+        Worker worker = factory.newWorker(TASK_QUEUE);
+
+        worker.registerWorkflowImplementationTypes(MyWorkflowImpl.class);
+
+        worker.registerActivitiesImplementations(new MyActivitiesImpl());
+
+        factory.start();
+
+        // Create the workflow client stub. It is used to start our workflow execution.
+        MyWorkflow workflow =
+                client.newWorkflowStub(
+                        MyWorkflow.class,
+                        WorkflowOptions.newBuilder()
+                                .setWorkflowId(WORKFLOW_ID)
+                                .setTaskQueue(TASK_QUEUE)
+                                .build());
+
+
+        WorkflowClient.start(workflow::run, "World");
+
+
+        // wait for a few seconds to let the local activity start and start the external workflow
+        try {
+            Thread.sleep(2_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // send a signal to the workflow
+        workflow.signal();
+
+        // wait for workflow completion
+        client.newUntypedWorkflowStub(WORKFLOW_ID).getResult(String.class);
+
+        System.exit(0);
+    }
+}
