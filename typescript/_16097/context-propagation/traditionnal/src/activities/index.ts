77d286933827fd58b6b767feeb44dc70da48abb9
Antonio Mendoza Pérez <antmendoza@gmail.com>
2025-09-26 15:15:16 +0200

    wip

--- typescript/_16097/context-propagation/traditionnal/src/activities/index.ts
+++ typescript/_16097/context-propagation/traditionnal/src/activities/index.ts
@@ -1,8 +1,30 @@
-import { log } from '@temporalio/activity';
+import { ApplicationFailure, log } from '@temporalio/activity';
 import { getContext } from '../context/context-injection';
 
 export async function extractCustomerNameFromContext(): Promise<string> {
   const propagatedContext = getContext();
   log.info(`Log from activity with customer ${propagatedContext.customer ?? 'unknown'}`);
   return `Hello, ${propagatedContext.customer}!`;
+}
+
+
+
+
+
+export async function localToModifyCustomerName(): Promise<string> {
+
+  getContext().customer = "localToModifyCustomerName";
+
+  const propagatedContext = getContext();
+
+  log.info(`Log from activity with customer ${propagatedContext.customer ?? 'unknown'}`);
+
+  return `Hello, ${propagatedContext.customer}!`;
+}
+
+
+
+
+export async function throwError(): Promise<string> {
+  throw ApplicationFailure.nonRetryable("This is a non-retryable error from activity");
 }
--- typescript/_16097/context-propagation/traditionnal/src/workflows/index.ts
+++ typescript/_16097/context-propagation/traditionnal/src/workflows/index.ts
@@ -1,10 +1,21 @@
-import { log, proxyActivities, workflowInfo } from '@temporalio/workflow';
+import { log, proxyActivities, proxyLocalActivities, workflowInfo } from '@temporalio/workflow';
 import type * as activities from '../activities';
 import { getContext, withContext } from '../context/workflow-interceptors';
 
 
-const { extractCustomerNameFromContext } = proxyActivities<typeof activities>({
+const { extractCustomerNameFromContext,
+  throwError,
+} = proxyActivities<typeof activities>({
   startToCloseTimeout: '5 minutes',
+});
+
+
+
+
+const {
+  localToModifyCustomerName
+} = proxyLocalActivities<typeof activities>({
+  startToCloseTimeout: '5 seconds',
 });
 
 export async function sampleWorkflow(): Promise<void> {
@@ -18,6 +29,14 @@ export async function sampleWorkflow(): Promise<void> {
   const afterUpdatingContextInWorkflow = await extractCustomerNameFromContext();
 
   const withContextValue=  await withContext({ customer: 'vip-123' }, async () => { return await extractCustomerNameFromContext(); })
+
+  log.info( clientContext + " | " + afterUpdatingContextInWorkflow + " | " + withContextValue + " | " + await extractCustomerNameFromContext());
+
+  await localToModifyCustomerName();
+
+  log.info( clientContext + " | " + afterUpdatingContextInWorkflow + " | " + withContextValue + " | " + await extractCustomerNameFromContext());
+
+  await throwError()
 
 
   log.info( clientContext + " | " + afterUpdatingContextInWorkflow + " | " + withContextValue + " | " + await extractCustomerNameFromContext());
